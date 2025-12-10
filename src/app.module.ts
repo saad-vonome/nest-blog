a0c0e462c956fd29071fbcad522a1f616bcc1af1
Saad Hossain <saad@vonome.com>
2025-12-10 15:50:30 +0600

    typeorm is also working

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { CategoriesModule } from './categories/categories.module';
-import { Category } from './categories/entities/category.entity';
-import { Post } from './posts/entities/post.entity';
 import { PostsModule } from './posts/posts.module';
-import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
 
 @Module({
@@ -23,8 +20,9 @@ import { UsersModule } from './users/users.module';
         username: config.get('DATABASE_USERNAME'),
         password: config.get('DATABASE_PASSWORD'),
         database: config.get('DATABASE_NAME'),
-        entities: [User, Category, Post],
-        synchronize: true, // Set to false in production
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        logging: true,
       }),
       inject: [ConfigService],
     }),
